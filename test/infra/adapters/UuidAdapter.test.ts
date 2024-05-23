1f9614951cb9ef06f61eab825cee586a513bece2
Ygor-=Evaldt-dev <evaldtygor@gmail.com>
2024-05-23 09:27:32 -0300

    garantido validação de uuid

--- test/infra/adapters/UuidAdapter.test.ts
+++ test/infra/adapters/UuidAdapter.test.ts
@@ -12,7 +12,23 @@ describe("uuid adapter", () => {
         const { uuid } = makeSut();
         const uuidV4 = uuid.generate();
 
+        const validUuidV4Length = 36;
+
         expect(uuidV4).toBeDefined();
-        expect(uuidV4.length).toBe(36);
+        expect(uuidV4.length).toBe(validUuidV4Length);
+    });
+
+    test("should return true if valid uuid", () => {
+        const { uuid } = makeSut();
+        const uuidV4 = uuid.generate();
+
+        expect(uuid.validate(uuidV4)).toBeTruthy();
+    });
+
+    test("should return false if invalid uuid", () => {
+        const { uuid } = makeSut();
+        const invalidUuidV4 = `${uuid.generate()}invalid`;
+
+        expect(uuid.validate(invalidUuidV4)).toBeFalsy();
     });
 });
