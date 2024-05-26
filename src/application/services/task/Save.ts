5d779889d6a983c9d33546bc6c60cf364a8a983a
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-05-26 10:22:29 -0300

    implementado caso de uso Save para tarefa

--- src/application/services/task/Save.ts
+++ src/application/services/task/Save.ts
@@ -1,16 +1,17 @@
-import TaskDto from "@/application/dtos/TaskDto";
+import Dto from "@/application/dtos/TaskDto";
 import IUseCase from "../IUseCase";
 import Task from "@/core/models/taks/Task";
 import ITaskRepository from "@/core/ports/repository-interfaces/ITaskRepository";
 
-export default class Save implements IUseCase<TaskDto, Task> {
+export default class Save implements IUseCase<Dto, Task> {
     constructor(
         private repository: ITaskRepository
     ) { }
 
-    execute(data: TaskDto): Promise<Task> {
-        throw new Error("Method not implemented.");
+    async execute({ title, finished, description }: Dto): Promise<Task> {
+        const task = new Task({ title, finished, description });
+        await this.repository.save(task);
+
+        return task;
     }
-
-
 }
