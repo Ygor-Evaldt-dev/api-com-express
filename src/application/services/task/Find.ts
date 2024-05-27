75b838daf0b9a52b34aaaf47a640614c38795441
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-05-27 06:49:55 -0300

    Construído caso de uso Find para tarefa

---
+++ src/application/services/task/Find.ts
@@ -0,0 +1,14 @@
+import ITaskRepository from "@/core/ports/repository-interfaces/ITaskRepository";
+import IUseCase from "../IUseCase";
+import Task from "@/core/models/taks/Task";
+
+export default class Find implements IUseCase<string, Promise<Task> | null> {
+    constructor(
+        private repository: ITaskRepository
+    ) { }
+
+    async execute(id: string): Promise<Promise<Task> | null> {
+        const task = await this.repository.find(id);
+        return task;
+    }
+}
