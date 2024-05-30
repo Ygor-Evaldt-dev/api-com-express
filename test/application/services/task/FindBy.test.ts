94d65c0ede03f5b90ce3824ee656d4f2f4d17161
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-05-30 08:13:33 -0300

    garantido encontrar apenas tarefas do usuário

--- test/application/services/task/FindBy.test.ts
+++ test/application/services/task/FindBy.test.ts
@@ -1,20 +1,39 @@
 import FindBy from "@/application/services/task/FindBy";
 import TaskLocalRepository from "@/infra/repositories/task/LocalRepository";
 import saveFiveTasksToTest from "../shared/saveFiveTasksToTest";
+import UserLocalRepository from "@/infra/repositories/user/LocalRepository";
+import Task from "@/core/models/task/Task";
+import ITaskRepository from "@/core/ports/repository-interfaces/ITaskRepository";
+import IUserRepository from "@/core/ports/repository-interfaces/IUserRepository";
+import User from "@/core/models/user/User";
 
 describe("find by", () => {
     function makeSut() {
+        const userRepository = new UserLocalRepository();
         const taskRepository = new TaskLocalRepository();
         const usecase = new FindBy(taskRepository);
 
         return ({
+            userRepository,
             taskRepository,
             usecase
         });
     }
 
+    async function deleteTestTasks(repository: ITaskRepository, tasks: Task[]): Promise<void> {
+        for (const task of tasks) {
+            await repository.delete(task.id.value);
+        }
+    }
+
+    async function deleteTestUser(repository: IUserRepository, users: User[]): Promise<void> {
+        for (const user of users) {
+            await repository.delete(user.id.value);
+        }
+    }
+
     test("should to find just user's tasks", async () => {
-        const { usecase, taskRepository } = makeSut();
+        const { usecase, taskRepository, userRepository } = makeSut();
         const { user1, user2, tasks } = await saveFiveTasksToTest();
 
         const userTasks = await usecase.execute({
@@ -22,6 +41,9 @@ describe("find by", () => {
             page: 0,
             take: 4
         });
+
+        await deleteTestTasks(taskRepository, tasks);
+        await deleteTestUser(userRepository, [user1, user2]);
 
         expect(userTasks.registers.length).toBe(3);
     });
