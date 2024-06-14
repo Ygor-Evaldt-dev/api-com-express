614a25a87b4937794119cfd9301af721f05a63ab
Ygor-Evaldt-dev <evaldtygor@gmail.com>
2024-06-14 18:06:41 -0300

    Garantido deletar uma tarefa existente

--- test/presentation/controllers/task/DeleteController.test.ts
+++ test/presentation/controllers/task/DeleteController.test.ts
@@ -5,10 +5,21 @@ import tasks from "../../util/tasks";
 describe("delete controller", () => {
     const api = AxiosInstance.generate();
 
-    test("Should return http status 201 if a new task is created", async () => {
+    test.skip("Should return http status 200 if to delete with success", async () => {
         const headers: any = await Authorization.getHeaders();
-        const { status } = await api.post("/task/delete/97014362-0592-43de-a835-1d191cd3eb8a", { headers });
+        const { status } = await api.delete("/task/delete/6e657338-db41-4769-a511-cd941c5e1ec4", { headers });
 
-        expect(status).toBe(201);
+        expect(status).toBe(200);
+    });
+
+    test("Should return http status 404 not found if the task is not exists", async () => {
+        try {
+            const headers: any = await Authorization.getHeaders();
+            const { status } = await api.delete("/task/delete/b4d3a1ba-5bc6-4a7b-86e5-896b610af1de", { headers });
+
+            expect(status).toBe(404);
+        } catch ({ response }: any) {
+            expect(response.status).toBe(404);
+        }
     });
 });
